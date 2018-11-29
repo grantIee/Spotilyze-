402bdfb6c78a0c2732b9e45f027655a547764597
yds05238 <jy392@cornell.edu>
2018-11-29 11:00:00 -0500

    sql-alchemy??

---
+++ src/api_db.py
@@ -0,0 +1,29 @@
+from flask_sqlalchemy import SQLAlchemy 
+db = SQLAlchemy()
+class User(db.Model):
+    __tablename__ = 'post'
+    id = db.Column(db.Integer, primary_key=True)
+    datas = db.relationship('Data', cascade='delete')
+
+    def __init__(self, **kwargs):
+        self.items = kwargs.get('items', '')
+
+    def serialize(self):
+        return {
+            'username': self.items
+        }
+
+class Data(db.Model):
+    __tablename__ = 'datas'
+    id = db.Column(db.Integer, primary_key=True)
+    items = db.Column(db.String, nullable=False)
+    postid = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+
+    def __init__(self, **kwargs):
+        self.items = kwargs.get('items', '')
+        self.postid = kwargs.get('postid')
+
+    def serialize(self):
+        return {
+            'playlists': self.items
+        }
